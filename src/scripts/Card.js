1dc48f0afeb3c80342e192dd25845a3e327c802b
Stanislava Filonova <stanislavafilonova@yandex.ru>
2021-11-10 23:53:41 +0300

    Подключили Webpack

---
+++ src/scripts/Card.js
@@ -0,0 +1,68 @@
+import { openPopup } from "./index.js";
+
+//Создание класса карточки
+export default class Card {
+  constructor(data, cardSelector) {
+    this._name = data.name;
+    this._link = data.link;
+    this._cardSelector = cardSelector;
+  }
+  //Возвращение шаблона новой карточки
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._cardSelector)
+      .content.querySelector(".element")
+      .cloneNode(true);
+
+    return cardElement;
+  }
+  // Метод подготовки карточки к публикации
+  generateCard() {
+    this._element = this._getTemplate();
+    this._element.querySelector(".element__photo").src = this._link;
+    this._element.querySelector(".element__name").textContent = this._name;
+    this._element.querySelector(".element__photo").alt = this._decription;
+
+    this._setEventListeners();
+    return this._element;
+  }
+
+  // Установка слушателей на элементы карточки
+  _setEventListeners() {
+    this._element
+      .querySelector(".element__like")
+      .addEventListener("click", () => {
+        this._toggleLike();
+      });
+    this._element
+      .querySelector(".element__delete")
+      .addEventListener("click", () => {
+        this._deleteElement();
+      });
+    this._element
+      .querySelector(".element__photo")
+      .addEventListener("click", () => {
+        this._openPlaceImageFullscreen();
+      });
+  }
+  //Открытие фото в полноэкранном режиме
+  _openPlaceImageFullscreen() {
+    const imagePopup = document.querySelector(".popup_type_image");
+    const imagePopupFullScreen = imagePopup.querySelector(".popup__image");
+    const imagePopupCaption = imagePopup.querySelector(".popup__caption");
+    imagePopupFullScreen.src = this._link;
+    imagePopupFullScreen.alt = `Фотография места. Название: ${this._name}`;
+    imagePopupCaption.textContent = this._decription;
+    openPopup(imagePopup);
+  }
+  // Переключение лайка в карточке
+  _toggleLike() {
+    this._element
+      .querySelector(".element__like")
+      .classList.toggle("element__like_active");
+  }
+  //Удаление карточки
+  _deleteElement() {
+    this._element.remove();
+  }
+}
